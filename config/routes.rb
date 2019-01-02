25fc19d7ba6acfcf5cb963f0e097ee2abdb7fecd
timhutch79git config --global user.name tim.hutch.work@gmail.com <tim.hutch.work@gmail.com>
2019-01-02 16:20:32 +1000

    added new employee forms need to format

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   resources :people
+
+
+
+
   devise_for :users
   root 'pages#home'
   get 'hr' => 'pages#hr'
   get 'it' => 'pages#it'
   get 'ohs' => 'pages#ohs'
-  get 'newuserform' => 'pages#newuserform.html.erb'
- 
+  get 'newuser' => 'pages#newuser'
+  get 'dashboard' => 'pages#dashboard.html.erb'
+  get 'form' => 'pages#form'
+  get 'people' => 'pages#people'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_01_060633) do
+ActiveRecord::Schema.define(version: 2019_01_02_041123) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.integer "user_id"
