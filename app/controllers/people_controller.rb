560342d0183203f282a2993a44964a9f776f9c8c
timhutch79git config --global user.name tim.hutch.work@gmail.com <tim.hutch.work@gmail.com>
2019-01-03 16:49:10 +1000

    people views completed not styled

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,9 +1,12 @@
 class PeopleController < InheritedResources::Base
 
+
+
+
   private
 
     def person_params
-      params.require(:person).permit(:user_id, :firstname, :lastname, :perferedname, :dateofbirth, :gender, :personalemail, :workemail, :mobile, :homephone, :workphone, :address1, :address2, :city, :postcode, :state, :country, :department, :office, :reportsto, :positiontitle, :startdate, :finsihdate, :customtext1, :customtext2, :customtext3, :customtext4, :customtext5, :customtext6, :customtext7, :customtext8, :customtext9, :customtext10, :customtext11, :customtext12, :customdate1, :customdate2, :customdate3, :customdate4, :customdate5)
+      params.require(:person).permit(:firstname, :lastname, :perferedname, :dateofbirth, :gender, :personalemail, :workemail, :mobile, :homephone, :workphone, :address1, :address2, :city, :postcode, :state, :country, :department, :office, :reportsto, :positiontitle, :startdate, :finsihdate, :customtext1, :customtext2, :customtext3, :customtext4, :customtext5, :customtext6, :customtext7, :customtext8, :customtext9, :customtext10, :customtext11, :customtext12, :customdate1, :customdate2, :customdate3, :customdate4, :customdate5)
     end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+
   resources :people
+
   ActiveAdmin.routes(self)
   devise_for :users
   as :user do 
@@ -11,5 +14,6 @@ Rails.application.routes.draw do
 	get 'about' => 'pages#about'
 	get 'contactus' => 'pages#contactus'
   get 'profile' => 'pages#profile'
+  get 'show' => 'people#id'
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
